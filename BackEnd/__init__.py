b44a90b5562ab34821f9a37534bcc525a40b0ad6
samanthaq1 <samanthaqiu3@gmail.com>
2025-04-06 08:41:05 -0500

    testing

--- BackEnd/__init__.py
+++ BackEnd/__init__.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="API key")
+genai.configure(api_key="AIzaSyAjAILDTv3M41p64ywtAoX3wCYBAzQvEIc")
 
 model = genai.GenerativeModel('gemini-2.0-flash')
 
